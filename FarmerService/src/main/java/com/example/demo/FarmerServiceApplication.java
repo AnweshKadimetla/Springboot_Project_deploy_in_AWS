1325578763b2e95275b2e19fd51bc6003b435f5b
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-10-06 14:10:10 +0530

    Add files via upload

--- FarmerService/src/main/java/com/example/demo/FarmerServiceApplication.java
+++ FarmerService/src/main/java/com/example/demo/FarmerServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableSwagger2
 public class FarmerServiceApplication {
 
